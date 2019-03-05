f6c28ff83c54363bd00468de76331b78445712e2
Ekaterina <cmlimm@Ekaterinas-MacBook-Air.local>
2019-03-05 15:52:31 +0300

    init commit

---
+++ HW_lesson_5/random_letter1.py
@@ -0,0 +1,15 @@
+import random
+
+words = ['самовар', 'весна', 'лето']
+word = random.choice(words)
+letterNumber = random.randint(0, len(word)-1)
+letter = word[letterNumber]
+wordQuestion = word[:letterNumber] + '?' + word[letterNumber+1:]
+
+print(wordQuestion)
+guess = input('Введите букву: ')
+if guess == letter:
+    print('Победа!')
+else:
+    print('Увы! Попробуйте в другой раз.')
+print('Слово: '+word)

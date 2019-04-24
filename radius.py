55a63f20c8fc228ca91adb495b77fe03c2f20cba
Ekaterina Motyleva <cmothlimm@gmail.com>
2019-04-24 23:56:36 +0300

    Добавлен просмотр результатов и поиск значений

--- radius.py
+++ radius.py
@@ -25,12 +25,17 @@ def volume():
     """
     try:
         result.set(round(4/3*pi*(radius.get()**3), 3))
-        unconnected_radius.set(radius.get()) #данная переменная защищает от случайного сохранения радиуса, который был введен, но не посчитан
+        unconnected_radius.set(radius.get()) #данная переменна защищает от случайного сохранения радиуса, который был введен, но не посчитан
     except Exception as err:
         error(err)
 
 def reader_txt():
-    pass
+    """
+    Чтение из html-файла
+    """
+    with open('volume_data.txt', 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+    return lines 
 
 def writer_txt():
     """
@@ -43,6 +48,60 @@ def writer_txt():
     except Exception as err:
         error(err)
 
+def look():
+    """
+    Окно с просмотром результата
+    """
+    try:
+        def check():
+            """
+            Функция для поиска
+            """
+            try:
+                text.tag_remove('found', '1.0', tkinter.END)
+                for word in list_of_words:
+                    idx = '1.0'
+                    while idx:
+                        idx = text.search(search_value.get(), idx,nocase=1, stopindex=tkinter.END)
+                        if idx:
+                            lastidx = '%s+%dc' % (idx, len(search_value.get()))
+                            text.tag_add('found', idx, lastidx)
+                            idx = lastidx
+                text.tag_config('found', foreground='red')
+            except Exception as err:
+                    error(err)
+        text_to_show = ''.join(reader_txt())
+        list_of_words = text_to_show.split()
+        print(list_of_words)
+        look_window = tkinter.Toplevel()
+        look_window.title('Просмотр результатов')
+
+        main_frame = tkinter.Frame(look_window)
+        main_frame.grid(row=0, column=0)
+        
+        search_frame = tkinter.Frame(main_frame)
+        search_frame.grid(row=0, column=0, sticky=tkinter.W)
+
+        search_label = tkinter.Label(search_frame, text='Введите строку: ')
+        search_label.grid(row=0, column=0, sticky=tkinter.E)
+
+        search_value = tkinter.StringVar()
+        search_entry = tkinter.Entry(search_frame, textvariable=search_value)
+        search_entry.grid(row=0, column=1)
+
+        search_button = tkinter.Button(search_frame,
+                                       text='Поиск',
+                                       command=check)
+        search_button.grid(row=0, column=2)
+        
+        text = tkinter.Text(main_frame)
+        text.grid(row=1, column=0)
+        text.insert(tkinter.INSERT, text_to_show)
+        
+        look_window.mainloop()
+    except Exception as err:
+        error(err)
+        
 def reader_html():
     """
     Чтение из html-файла
@@ -116,15 +175,20 @@ try:
     save_frame = tkinter.Frame(left_frame)
     save_frame.grid(row=2, column=0)
 
+    button_look = tkinter.Button(save_frame,
+                                 text='Результаты',
+                                 command=look)
+    button_look.grid(row=0, column=0)
+    
     button_save = tkinter.Button(save_frame,
                                  text='Сохранить',
                                  command=writer)
-    button_save.grid(row=0, column=0)
+    button_save.grid(row=0, column=1)
 
     where = tkinter.StringVar()
     where.set('Teкст')
     save_option = tkinter.OptionMenu(save_frame, where, 'Текст', 'HTML')
-    save_option.grid(row=0, column=1)
+    save_option.grid(row=0, column=2)
 
     info = tkinter.StringVar()
     announcer = tkinter.Label(save_frame, textvariable=info)

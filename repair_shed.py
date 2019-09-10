67a5ac25d01fababc4a5c242aa7974665f2c0f7c
Ekaterina <cmlimm@Ekaterinas-Air.localnet>
2019-09-10 12:41:41 +0300

    10th of September

--- repair_shed.py
+++ repair_shed.py
@@ -3,7 +3,7 @@ class SparePart():
         self.name = name
         self.cost = cost
         self.quantity = quantity
-
+    # общая стоимость деталей данного вида
     def total(self):
         return cost*quantity
 
@@ -14,9 +14,11 @@ class Car():
         self.year = year
         self.needed_parts = []
 
-    def define_needed_parts(self, part):
+    # добавление запчасти, необходимой для машины
+    def add_needed_part(self, part):
         self.needed_parts.append(part)
 
+    # получение общей стоимости запчастей для машины
     def get_cost_of_parts(self):
         return sum(list(map(lambda x: x.total, needed_parts)))
 
@@ -25,9 +27,9 @@ class Client():
         self.name = name
         self.address = address
         self.number = number
-
-    def client_car(self, car):
+    # создание машины клиента (возможно, стоит метод перенести в класс Manager)
+    def add_client_car(self, car):
         self.car = car
 
 class Manager():
-    def
+    pass

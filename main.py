f50a54f2643c12b663486e9e9b48c42b1d47aa59
Ekaterina <cmothlimm@gmail.com>
2019-11-04 23:36:24 +0300

    small tweak

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ dispatcher.add_handler(showkeywords_handler)
 dispatcher.add_handler(unknown_handler)
 
 # благодаря этой строчке проверяется наличие новостей каждые 3600 сек
-worker.run_repeating(check_for_updates, interval=3600, first=0, context=chat_id)
+worker.run_repeating(check_for_updates, interval=1500, first=0, context=chat_id)
 
 updater.start_polling()
 updater.idle()

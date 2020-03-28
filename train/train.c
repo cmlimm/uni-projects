e10b56648bec86c8cd43b60e58452f2cac0eb765
Ekaterina <cmothlimm@gmail.com>
2020-03-28 22:52:04 +0300

    solved empty strings problem

--- train/train.c
+++ train/train.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_IMMORTALS 10
+#define MAX_passengers 10
 
 typedef struct carriage {
-	char *number;
-    int n_immortals;
-    char *immortals[MAX_IMMORTALS];
-    char *isPotionHere;
+    char *number;
+    int n_passengers;
+    char *passengers[MAX_passengers];
+    char *isTreasureHere;
 	struct carriage * next;
 } carriage;
 
 void display(carriage *start){
 	carriage *i = start;
     int k;
+
 	for ( ; i != NULL; i=i->next){ /* i=(*i).next */
-        printf("Number of carriage: %s\n", i->number);
-		printf("Number of immortals: %d\nImmortals: ", i->n_immortals);
-        for (k = 0; k < i->n_immortals; k++){
-            if (k != i->n_immortals - 1)
-                printf("%s, ", i->immortals[k]);
+
+        printf("Number of carriage: ");
+        printf("%s\n", i->number);
+
+		printf("Number of passengers: %d\nPassengers: ", i->n_passengers);
+        for (k = 0; k < i->n_passengers; k++){
+            if (k != i->n_passengers - 1)
+                printf("%s, ", i->passengers[k]);
             else
-                printf("%s\n", i->immortals[k]);
+                printf("%s\n", i->passengers[k]);
         }
-        printf("Is Potion of Immortality in this carriage? ");
-        printf("%s\n", i->isPotionHere);
+        printf("Is Treasure in this carriage? ");
+        printf("%s\n", i->isTreasureHere);
 	}
 }
 
@@ -32,28 +36,31 @@ carriage *add_carriage(){
 	carriage *new = malloc(sizeof(carriage));
     int i, n;
     char name[30];
-    char new_number[30];
-    char flag[4];
+    char isTreasureHere[4];
+    char number[30];
 
     printf("Enter number of carriage: ");
-    scanf("%29s", new_number);
-    new->number = new_number;
+    scanf("%29s", number);
+    new->number = malloc(strlen(number) + 1);
+    strcpy(new->number, number);
 
-    printf("Enter number of immortal: ");
+    printf("Enter number of passengers: ");
     scanf("%d", &n);
-    new->n_immortals = n;
+    new->n_passengers = n;
 
     if (n != 0){
-        printf("Enter names of immortals: ");
-        for (i = 0; i < new->n_immortals; i++){
+        printf("Enter names of passengers: ");
+        for (i = 0; i < new->n_passengers; i++){
             scanf("%29s", name);
-            new->immortals[i] = malloc(strlen(name) + 1);
-            strcpy(new->immortals[i], name);
+            new->passengers[i] = malloc(strlen(name) + 1);
+            strcpy(new->passengers[i], name);
         }
     }
-    printf("Is Potion of Immortality in this carriage? ");
-    scanf("%3s", flag);
-    new->isPotionHere = flag;
+
+    printf("Is Treasure in this carriage? ");
+    scanf("%3s", isTreasureHere);
+    new->isTreasureHere = malloc(strlen(isTreasureHere) + 1);
+    strcpy(new->isTreasureHere, isTreasureHere);
 
     new->next = NULL;
 
@@ -61,16 +68,17 @@ carriage *add_carriage(){
 }
 
 void release(carriage *start){
-
 	carriage *i = start;
 	carriage *next = NULL;
     int k;
 
 	for ( ; i != NULL ; i = next){
 		next = i->next;
-        for (k = 0; k < i->n_immortals; k++){
-            free(i->immortals[k]);
+        for (k = 0; k < i->n_passengers; k++){
+            free(i->passengers[k]);
         }
+        free(i->number);
+        free(i->isTreasureHere);
 		free(i);
 	}
 }
@@ -79,7 +87,6 @@ int main(){
 	carriage *start = NULL;
 
 	start = add_carriage();
-
 	display(start);
 	release(start);
 

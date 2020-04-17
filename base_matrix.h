d493b2bf517cac77c90d042929c4cbfb67d94f9b
Ekaterina <cmothlimm@gmail.com>
2020-04-17 22:19:54 +0300

    small fix

--- base_matrix.h
+++ base_matrix.h
@@ -18,12 +18,12 @@ void matrix_deallocate(matrix *mtrx);
 void matrix_fill(matrix *mtrx, double value);
 void matrix_show(matrix *mtrx);
 
-void matrix_random(matrix *mtrx, int min, int max) /* not implemented */
-void matrix_identity(matrix *mtrx) /* not implemented */
-matrix *matrix_add(matrix *mtrx1, matrix *mtrx2) /* not implemented */
-matrix *matrix_dot(matrix *mtrx1, matrix *mtrx2) /* not implemented */
-matrix *matrix_mult(matrix *mtrx1, int a) /* not implemented */
-matrix *matrix_transpose(matrix *mtrx1) /* not implemented */
+void matrix_random(matrix *mtrx, int min, int max); /* not implemented */
+void matrix_identity(matrix *mtrx); /* not implemented */
+matrix *matrix_add(matrix *mtrx1, matrix *mtrx2); /* not implemented */
+matrix *matrix_dot(matrix *mtrx1, matrix *mtrx2); /* not implemented */
+matrix *matrix_mult(matrix *mtrx1, int a); /* not implemented */
+matrix *matrix_transpose(matrix *mtrx1); /* not implemented */
 matrix *matrix_row(int index); /* not implemented */
 matrix *matrix_column(int index); /* not implemented */
 double matrix_det(matrix *mtrx); /* not implemented */

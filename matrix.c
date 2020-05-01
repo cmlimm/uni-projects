ed3aa584d51da304b91e053e777c99ebeadfbcc9
Ekaterina <cmothlimm@gmail.com>
2020-05-02 00:06:23 +0300

    commented everything

--- matrix.c
+++ matrix.c
@@ -1,24 +1,34 @@
 #include "Python.h"
 #include "matrix.h"
-// #include "base_matrix.h"
 
+/*
+ * Function: PyObjectToMatrixObject
+ * --------------------------------
+ * converts PyList to MatrixObject
+ *
+ * returns: MatrixObject
+ */
 static MatrixObject *PyObjectToMatrixObject(PyObject *pMatrix){
     PyObject *pMatrixSub;
     PyObject *pItem;
     Py_ssize_t rows, cols;
-    MatrixObject *initial;
+    MatrixObject *matrix;
     int i, j;
     double item;
 
     rows = PyList_Size(pMatrix);
+    // get first row and calculate it's length
     pMatrixSub = PyList_GetItem(pMatrix, 0);
     cols = PyList_Size(pMatrixSub);
 
-    initial = c_matrix_allocate(rows, cols);
+    // allocate memory for MatrixObject
+    matrix = c_matrix_allocate(rows, cols);
     for (i = 0; i < rows; i++) {
+        // get i_th row
         pMatrixSub = PyList_GetItem(pMatrix, i);
 
         for (j = 0; j < cols; j++) {
+            // get pMatrix[i][j]
             pItem = PyList_GetItem(pMatrixSub, j);
 
             if(!PyFloat_Check(pItem)){
@@ -26,187 +36,203 @@ static MatrixObject *PyObjectToMatrixObject(PyObject *pMatrix){
                 return NULL;
             }
 
+            // write PyList[i][j] to MatrixObject[i][j]
             item = PyFloat_AsDouble(pItem);
-            initial->values[i][j] = item;
+            matrix->values[i][j] = item;
         }
     }
 
-    return initial;
+    return matrix;
 }
 
+/*
+ * Function: MatrixObjectToPyObject
+ * --------------------------------
+ * converts MatrixObject to PyList
+ *
+ * returns: PyList
+ */
 static PyObject *MatrixObjectToPyObject(MatrixObject* matrix){
     PyObject *pList, *pSublist;
     PyObject *pItem;
     Py_ssize_t rows, cols;
     int i, j;
 
+    // get dimensions of matrix
     rows = matrix->rows;
     cols = matrix->columns;
 
+    // create main list with empty rows
     pList = PyList_New(rows);
     for (i = 0; i < rows; i++) {
+        // create row
         pSublist = PyList_New(cols);
         for (j = 0; j < cols; j++) {
+            // write MatrixObject[i][j] to PyList[i][j]
             pItem = Py_BuildValue("d", matrix->values[i][j]);
             PyList_SetItem(pSublist, j, pItem);
         }
+        // write row to Pylist
         PyList_SetItem(pList, i, pSublist);
     }
 
     return pList;
 }
 
+/*
+ * Function: fill
+ * --------------
+ * fills PyList with specified value
+ *
+ * returns: PyList
+ */
 static PyObject *fill(PyObject *self, PyObject *args){
-    PyObject *pValue;
+    MatrixObject *matrix;
     PyObject *pList;
-    PyObject *pSublist;
-    int i, j, rows, cols;
+    int rows, cols;
     double value;
 
+    // check parameters
     if (!PyArg_ParseTuple(args, "iid", &rows, &cols, &value)) {
         PyErr_SetString(PyExc_TypeError, "Parameters must be (int, int, float).");
         return NULL;
     }
 
-    pValue = Py_BuildValue("d", value);
+    matrix = c_matrix_fill(rows, cols, value);
+    pList = MatrixObjectToPyObject(matrix);
 
-    pList = PyList_New(rows);
-    for (i = 0; i < rows; i++) {
-        pSublist = PyList_New(cols);
-        for (j = 0; j < cols; j++) {
-            PyList_SetItem(pSublist, j, pValue);
-        }
-        PyList_SetItem(pList, i, pSublist);
-    }
+    c_matrix_deallocate(matrix);
 
     return pList;
 }
 
+/*
+ * Function: randfill
+ * ------------------
+ * fills PyList with random values
+ *
+ * returns: PyList
+ */
 static PyObject *randfill(PyObject *self, PyObject *args){
-    PyObject *pValue;
+    MatrixObject *matrix;
     PyObject *pList;
-    PyObject *pSublist;
-    int i, j, rows, cols;
+    int rows, cols;
     double min, max;
 
-    srand(time(NULL));
-
+    //check parameters
     if (!PyArg_ParseTuple(args, "iidd", &rows, &cols, &min, &max)) {
         PyErr_SetString(PyExc_TypeError, "Parameters must be (int, int, float, float).");
         return NULL;
     }
 
-    pList = PyList_New(rows);
-    for (i = 0; i < rows; i++) {
-        pSublist = PyList_New(cols);
-        for (j = 0; j < cols; j++) {
-            pValue = Py_BuildValue("d", RandomReal(min, max));
-            PyList_SetItem(pSublist, j, pValue);
-        }
-        PyList_SetItem(pList, i, pSublist);
-    }
+    matrix = c_matrix_random(rows, cols, min, max);
+    pList = MatrixObjectToPyObject(matrix);
+
+    c_matrix_deallocate(matrix);
 
     return pList;
 }
 
+/*
+ * Function: identity
+ * ------------------
+ * creates identity matrix in form of Pylist
+ *
+ * returns: PyList
+ */
 static PyObject *identity(PyObject *self, PyObject *args){
-    PyObject *pOne, *pZero;
+    MatrixObject *matrix;
     PyObject *pList;
-    PyObject *pSublist;
-    int i, j, rows;
+    int rows;
 
+    //check parameters
     if (!PyArg_ParseTuple(args, "i", &rows)) {
         PyErr_SetString(PyExc_TypeError, "Parameter must be int.");
         return NULL;
     }
 
-    pOne = Py_BuildValue("d", 1.0);
-    pZero = Py_BuildValue("d", 0.0);
+    matrix = c_matrix_identity(rows);
+    pList = MatrixObjectToPyObject(matrix);
 
-    pList = PyList_New(rows);
-    for (i = 0; i < rows; i++) {
-        pSublist = PyList_New(rows);
-        for (j = 0; j < rows; j++) {
-            if (i == j)
-                PyList_SetItem(pSublist, j, pOne);
-            else
-                PyList_SetItem(pSublist, j, pZero);
-        }
-        PyList_SetItem(pList, i, pSublist);
-    }
+    c_matrix_deallocate(matrix);
 
     return pList;
 }
 
+/*
+ * Function: add
+ * -------------
+ * sums two Pylist like matrixes
+ * args: [[1.0, 1.0],[2.0, 2.0]], [[2.0, 2.0],[1.0, 1.0]]
+ * result: [[3.0, 3.0],[3.0, 3.0]]
+ *
+ * returns: PyList
+ */
 static PyObject* matrix_add(PyObject *self, PyObject *args){
-    return matrix_add_common(self, args, 1);
-}
-
-static PyObject* matrix_sub(PyObject *self, PyObject *args){
-    return matrix_add_common(self, args, -1);
-}
-
-static PyObject* matrix_add_common(PyObject *self, PyObject *args, int sign){
     PyObject *pMatrix1, *pMatrix2;
-    PyObject *pMatrixSub1, *pMatrixSub2;
-    PyObject *pItem1, *pItem2, *pItem;
-    PyObject *pList, *pSublist;
-    Py_ssize_t rows1, rows2, cols1, cols2;
-    int i, j;
-    float item1, item2, item;
+    MatrixObject *matrix1, *matrix2;
+    MatrixObject *result;
+    PyObject *pList;
 
+    // check parameters
     if (!PyArg_ParseTuple(args, "O!O!", &PyList_Type, &pMatrix1, &PyList_Type, &pMatrix2)) {
         PyErr_SetString(PyExc_TypeError, "Parameters must be (list, list).");
         return NULL;
     }
 
-    rows1 = PyList_Size(pMatrix1);
-    rows2 = PyList_Size(pMatrix2);
-    if(rows1 != rows2) {
-        PyErr_SetString(PyExc_ValueError, "Shapes of the matrixes must be the same.");
-        return NULL;
-    }
-
-    pList = PyList_New(rows1);
-    for (i = 0; i < rows1; i++) {
-        pMatrixSub1 = PyList_GetItem(pMatrix1, i);
-        pMatrixSub2 = PyList_GetItem(pMatrix2, i);
-
-        cols1 = PyList_Size(pMatrixSub1);
-        cols2 = PyList_Size(pMatrixSub1);
-
-        if(cols1 != cols2) {
-            PyErr_SetString(PyExc_ValueError, "Shapes of the matrixes must be the same.");
-            return NULL;
-        }
-
-        pSublist = PyList_New(cols1);
-        for (j = 0; j < cols1; j++) {
-            pItem1 = PyList_GetItem(pMatrixSub1, j);
-            pItem2 = PyList_GetItem(pMatrixSub2, j);
-
-            if(!PyFloat_Check(pItem1) && !PyFloat_Check(pItem2)){
-                PyErr_SetString(PyExc_ValueError, "List items must by float.");
-                return NULL;
-            }
-
-            item1 = PyFloat_AsDouble(pItem1);
-            item2 = PyFloat_AsDouble(pItem2);
-            item = item1 + sign*item2;
-
-            pItem = Py_BuildValue("d", item);
-            PyList_SetItem(pSublist, j, pItem);
-        }
-        PyList_SetItem(pList, i, pSublist);
-    }
+    matrix1 = PyObjectToMatrixObject(pMatrix1);
+    matrix2 = PyObjectToMatrixObject(pMatrix2);
+    result = c_matrix_add(matrix1, matrix2);
+    pList = MatrixObjectToPyObject(result);
 
     return pList;
 }
 
+/*
+ * Function: sub
+ * -------------
+ * subtracts two Pylist like matrixes
+ * args: [[1.0, 1.0],[2.0, 2.0]], [[2.0, 2.0],[1.0, 1.0]]
+ * result: [[-1.0, -1.0],[1.0, 1.0]]
+ *
+ * returns: PyList
+ */
+static PyObject* matrix_sub(PyObject *self, PyObject *args){
+    PyObject *pMatrix1, *pMatrix2;
+    MatrixObject *matrix1, *matrix2;
+    MatrixObject *result;
+    PyObject *pList;
+
+    // check parameters
+    if (!PyArg_ParseTuple(args, "O!O!", &PyList_Type, &pMatrix1, &PyList_Type, &pMatrix2)) {
+        PyErr_SetString(PyExc_TypeError, "Parameters must be (list, list).");
+        return NULL;
+    }
+
+    matrix1 = PyObjectToMatrixObject(pMatrix1);
+    matrix2 = PyObjectToMatrixObject(pMatrix2);
+    result = c_matrix_sub(matrix1, matrix2);
+    pList = MatrixObjectToPyObject(result);
+
+    return pList;
+}
+
+/*
+ * Function: transpose
+ * -------------------
+ * transposes PyList like matrix
+ * args: [[1.0, 2.0],
+ *        [3.0, 4.0]]
+ * result: [[1.0, 3.0],
+ *          [2.0, 4.0]]
+ *
+ * returns: PyList
+ */
 static PyObject* matrix_transpose(PyObject *self, PyObject *args) {
     PyObject *pMatrix, *pList;
     MatrixObject *initial, *result;
 
+    // check parameters
     if (!PyArg_ParseTuple(args, "O!", &PyList_Type, &pMatrix)) {
         PyErr_SetString(PyExc_TypeError, "Parameter must be a list.");
         return NULL;
@@ -222,6 +248,7 @@ static PyObject* matrix_transpose(PyObject *self, PyObject *args) {
 }
 
 static PyMethodDef MatrixMethods[] = {
+    //{"name of function", function, type of args, doc string}
     {"fill", (PyCFunction)fill, METH_VARARGS,
      "Returns matrix filled with certain value"},
     {"randfill", (PyCFunction)randfill, METH_VARARGS,
@@ -238,11 +265,11 @@ static PyMethodDef MatrixMethods[] = {
 };
 
 static struct PyModuleDef matrixmanmodule = {
-    PyModuleDef_HEAD_INIT,
-    .m_name = "matrixman",
-    .m_doc = "Module dedicated to working with matrixes",
-    .m_size = -1,
-    .m_methods = MatrixMethods
+    PyModuleDef_HEAD_INIT, // ???
+    .m_name = "matrixman", // mosule name
+    .m_doc = "Module dedicated to working with matrixes", // doc string
+    .m_size = -1, // ???
+    .m_methods = MatrixMethods // struct with methods
 };
 
 PyMODINIT_FUNC PyInit_matrixman(void)

588b0c01fe1b18fb002a0770056ba8601f30ee3e
Ekaterina <cmothlimm@gmail.com>
2020-07-11 19:34:24 +0300

    extracted manipulate into separate file

--- workshops/lesson2_visualisation/extremum_bisection.py
+++ workshops/lesson2_visualisation/extremum_bisection.py
@@ -1,6 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button, RadioButtons
+from manipulate import Manipulate
 
 def function(x):
     return x**2+x**4
@@ -40,46 +38,6 @@ def bisection(function, lower, upper, max_iter, precision):
         iterations += 1
 
     return states
-
-class Manipulate():
-    def __init__(self, function, bounds, states):
-        self.states = states
-
-        self.fig, self.ax = plt.subplots()
-        plt.subplots_adjust(left=0.25, bottom=0.25)
-
-        # создаем списки с точками графика функции
-        self.display_range = np.arange(bounds[0], bounds[1], 0.01)
-        self.display_func = list(map(lambda x: function(x), self.display_range))
-
-        # создаем графики функции, точки экстремума, границ
-        self.plotted, = plt.plot(self.display_range, self.display_func, lw=2)
-        self.initial_extr = plt.scatter([states[0][2]],[function(states[0][2])], color='red')
-        self.lower_border = plt.axvline(x=states[0][0], color='red')
-        self.upper_border = plt.axvline(x=states[0][1], color='red')
-
-        self.ax.margins(x=0)
-        axsteps = plt.axes([0.25, 0.15, 0.65, 0.03])
-
-        # создаем слайдер для изменения шага
-        steps = len(states)
-        self.step_slider = Slider(axsteps, 'Step', 0, steps - 1, valinit=0, valfmt='%0.0f')
-
-        self.step_slider.on_changed(self.update)
-
-        plt.show()
-
-    # функция изменения графика при изменении шага
-    def update(self, val):
-        # получаем состояние на текущем шаге
-        state = self.states[int(self.step_slider.val)]
-
-        # изменяем точку экстремума и границы
-        self.initial_extr.set_offsets([state[2], function(state[2])])
-        self.lower_border.set_xdata(state[0])
-        self.upper_border.set_xdata(state[1])
-
-        self.fig.canvas.draw_idle()
 
 # начальные данные
 lower = -5
--- workshops/lesson2_visualisation/minimum_golden_section.py
+++ workshops/lesson2_visualisation/minimum_golden_section.py
@@ -1,6 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button, RadioButtons
+from manipulate import Manipulate
 
 def function(x):
     return x**2+x**4
@@ -15,7 +13,7 @@ def golden_section(function, lower, upper, max_iter, precision):
     """
 
     iterations = 0
-    states = [(lower, upper)]
+    states = [(lower, upper, (lower + upper)/2)]
 
     while abs(lower - upper) > precision and iterations <= max_iter:
 
@@ -32,49 +30,11 @@ def golden_section(function, lower, upper, max_iter, precision):
         else:
             upper = upper_section
 
-        states.append((lower, upper))
+        states.append((lower, upper, (lower + upper)/2))
 
         iterations += 1
 
     return states
-
-class Manipulate():
-    def __init__(self, function, bounds, states):
-        self.states = states
-
-        self.fig, self.ax = plt.subplots()
-        plt.subplots_adjust(left=0.25, bottom=0.25)
-
-        # создаем списки с точками графика функции
-        self.display_range = np.arange(bounds[0], bounds[1], 0.01)
-        self.display_func = list(map(lambda x: function(x), self.display_range))
-
-        # создаем графики функции и границ
-        self.plotted, = plt.plot(self.display_range, self.display_func, lw=2)
-        self.lower_border = plt.axvline(x=states[0][0], color='red')
-        self.upper_border = plt.axvline(x=states[0][1], color='red')
-
-        self.ax.margins(x=0)
-        axsteps = plt.axes([0.25, 0.15, 0.65, 0.03])
-
-        # создаем слайдер для изменения шага
-        steps = len(states)
-        self.step_slider = Slider(axsteps, 'Step', 0, steps - 1, valinit=0, valfmt='%0.0f')
-
-        self.step_slider.on_changed(self.update)
-
-        plt.show()
-
-    # функция изменения графика при изменении шага
-    def update(self, val):
-        # получаем состояние на текущем шаге
-        state = self.states[int(self.step_slider.val)]
-
-        # изменяем границы
-        self.lower_border.set_xdata(state[0])
-        self.upper_border.set_xdata(state[1])
-
-        self.fig.canvas.draw_idle()
 
 # начальные данные
 lower = -5
